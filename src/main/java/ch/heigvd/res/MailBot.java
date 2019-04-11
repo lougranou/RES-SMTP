41dfa622f92ec19d50442467ad685749431f9427
Didier Page <page.didier@gmail.com>
2019-04-11 19:38:55 +0200

    Clean the main, but not enough

--- src/main/java/ch/heigvd/res/MailBot.java
+++ src/main/java/ch/heigvd/res/MailBot.java
@@ -17,19 +17,19 @@ public class MailBot {
 
         Config config = new Config("./src/main/java/ch/heigvd/res/config/config.txt");
         SmtpClient client = new SmtpClient(config.getServerAdress(),config.getServerPort());
-
-
-        String pathToFile = "./src/main/java/ch/heigvd/res/config/emails.txt";
-        LinkedList<Person> victims = Person.parseFile(pathToFile);
+        LinkedList<Person> victims = Person.parseFile( "./src/main/java/ch/heigvd/res/config/emails.txt");
 
         ArrayList<Group> groups = new ArrayList<>();
-        for (int i =0;i< config.getNbGroup();i++)
-            groups.add(new Group());
-        for(int i = 0; i< victims.size();i++)
-            groups.get(i % config.getNbGroup()).addPerson(victims.get(1));
 
-        pathToFile = "./src/main/java/ch/heigvd/res/config/prankMessage.txt";
-        LinkedList<Message> messages = Message.parseFile(pathToFile);
+        //HACK
+        for (int i =0;i< config.getNbGroup();i++){
+            groups.add(new Group());
+        }
+        for(int i = 0; i< victims.size();i++){
+            groups.get(i % config.getNbGroup()).addPerson(victims.get(1));
+        }
+
+        LinkedList<Message> messages = Message.parseFile("./src/main/java/ch/heigvd/res/config/prankMessage.txt");
         LinkedList<Mail> mails = new LinkedList<>();
         for (Group g : groups) {
             mails.add(new Mail(g.getSenderVictimAddress(), g.getReciversVictimAddress(), null, null, messages.get(0)));
